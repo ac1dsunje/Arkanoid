3d77b194b911fbfc72f31db698154793da717805
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-08 14:50:28 +0200

    added bouncing ball

--- Assets/_Game/Scripts/EntryPoint.cs
+++ Assets/_Game/Scripts/EntryPoint.cs
@@ -9,16 +9,26 @@ public class EntryPoint: MonoBehaviour
     [SerializeField] private CameraController _cam;
     [SerializeField] private BordersSpawnerConfig _bordersSpawnerConfig;
     [SerializeField] private BordersSpawner _bordersSpawner;
+    [SerializeField] private GameObject _ballPrefab;
+
+    private Transform _platform;
 
     private void Awake()
     {
         _bordersSpawner.Construct(_bordersSpawnerConfig, _cam.GetWidth(), _cam.GetHeight());
         CreatePlatform();
+        CreateBall();
     }
 
     private void CreatePlatform()
     {
-        Instantiate(_platformPrefab, new Vector2(0, -_cam.GetHeight() + 1f), Quaternion.identity);
+        _platform = Instantiate(_platformPrefab, new Vector2(0, -_cam.GetHeight() + 1f), Quaternion.identity)
+            .GetComponent<Transform>();
+    }
+
+    private void CreateBall()
+    {
+        Instantiate(_ballPrefab, new Vector2(0, _platform.position.y + 1f), Quaternion.identity);
     }
 }
 }
