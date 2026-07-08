259e7c4c9c650ae615afcd12b08066fad0135739
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-08 14:18:06 +0200

    dynamic spawn positions and scale

---
+++ Assets/_Game/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Game.Scripts
+{
+public class CameraController: MonoBehaviour
+{
+    private Camera _cam;
+
+    private void Awake()
+    {
+        _cam  = GetComponent<Camera>();
+    }
+
+    public float GetHeight()
+    {
+        return _cam.orthographicSize;
+    }
+
+    public float GetWidth()
+    {
+        return _cam.aspect * _cam.orthographicSize;
+    }
+}
+}
--- Assets/_Game/Scripts/EntryPoint.cs
+++ Assets/_Game/Scripts/EntryPoint.cs
@@ -5,9 +5,8 @@ namespace _Game.Scripts
 public class EntryPoint: MonoBehaviour
 {
     [SerializeField] private GameObject _platformPrefab;
-    [SerializeField] private Vector2 _spawnPoint;
     [SerializeField] private GameObject _borderPrefab;
-    [SerializeField] private float _xOffset = 5f;
+    [SerializeField] private CameraController _cam;
 
     private void Awake()
     {
@@ -17,13 +16,23 @@ public class EntryPoint: MonoBehaviour
 
     private void CreateBorders()
     {
-        Instantiate(_borderPrefab, new Vector2(_spawnPoint.x + _xOffset, _spawnPoint.y), Quaternion.identity);
-        Instantiate(_borderPrefab, new Vector2(_spawnPoint.x - _xOffset, _spawnPoint.y), Quaternion.identity);
+        var xOffset = _cam.GetWidth();
+        var yScale = _cam.GetHeight() * 2;
+
+        CreateBorder(xOffset, yScale);
+        CreateBorder(-xOffset, yScale);
+    }
+
+    private void CreateBorder(float xOffset, float yScale)
+    {
+        var border = Instantiate(_borderPrefab, new Vector2(xOffset, 0), Quaternion.identity)
+            .GetComponent<Transform>();
+        border.localScale = new(border.localScale.x, yScale, border.localScale.z);
     }
 
     private void CreatePlatform()
     {
-        Instantiate(_platformPrefab, _spawnPoint, Quaternion.identity);
+        Instantiate(_platformPrefab, new Vector2(0, -_cam.GetHeight() + 1f), Quaternion.identity);
     }
 }
 }
