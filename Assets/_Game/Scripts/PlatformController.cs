47d8f0f887ae5591d736391399960d57c6801bc7
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-08 13:48:57 +0200

    rider config update and moving platform

---
+++ Assets/_Game/Scripts/PlatformController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace _Game.Scripts
+{
+public class PlatformController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private Rigidbody2D _rb;
+    
+    private float _velocity;
+    
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _velocity = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.linearVelocityX = _velocity * _moveSpeed;
+    }
+}
+}
