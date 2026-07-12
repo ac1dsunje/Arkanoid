38eb394624bc71bc706e9cd8d909b1df2c6a895b
ac1dsunje <ar.cherba63programming@gmail.com>
2026-07-12 16:30:04 +0200

    added coordinator class

---
+++ Assets/_Game/Scripts/Coordinator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace _Game.Scripts
+{
+public class Coordinator
+{
+    public int BlockPairs { get; private set; }
+    public float BorderCenterX { get; private set; }
+    
+    public Coordinator(float camWidth, float blockSize, float borderWidth)
+    {
+        BlockPairs = CalculateBlockPairs(camWidth, borderWidth, blockSize);
+        BorderCenterX = CalculateBorderCenterX(BlockPairs, blockSize, borderWidth);
+    }
+
+    private int CalculateBlockPairs(float camWidth, float borderWidth, float blockSize)
+    {
+        var availableWidth = camWidth - (2 * borderWidth);
+        return Mathf.FloorToInt(availableWidth / (2 * blockSize));
+    }
+
+    private float CalculateBorderCenterX(float  blockPairs, float borderWidth, float blockSize)
+    {
+        return blockPairs * blockSize + (borderWidth / 2f);
+    }
+}
+}
--- Assets/_Game/Scripts/EntryPoint.cs
+++ Assets/_Game/Scripts/EntryPoint.cs
@@ -27,27 +27,15 @@ namespace _Game.Scripts
             var camWidth = _cameraController.GetWidth();
             var camHeight = _cameraController.GetHeight();
 
-            var blockPairs = GetBlockPairs(camWidth, _borderWidth, _blockSize);
-            var borderCenterX = GetBorderCenterX(blockPairs, _blockSize, _borderWidth);
+            var coordinator = new Coordinator(camWidth, _blockSize, _borderWidth);
 
-            _bordersSpawner.Construct(_bordersSpawnerConfig, borderCenterX, camHeight);
-            _blocksSpawner.Construct(_blockPrefabs, blockPairs, camHeight, _blockSize);
+            _bordersSpawner.Construct(_bordersSpawnerConfig, coordinator.BorderCenterX, camHeight);
+            _blocksSpawner.Construct(_blockPrefabs, coordinator.BlockPairs, camHeight, _blockSize);
             
             var startPosition = new Vector2(0, -camHeight + 1f);
             _platform.Construct(startPosition);
             
             _ball.Construct(_platform.transform);
-        }
-
-        private int GetBlockPairs(float camWidth, float borderWidth, float blockSize)
-        {
-            var availableWidth = camWidth - (2 * borderWidth);
-            return Mathf.FloorToInt(availableWidth / (2 * blockSize));
-        }
-
-        private float GetBorderCenterX(int blockPairs, float blockSize, float borderWidth)
-        {
-            return blockPairs * blockSize + (borderWidth / 2f);
         }
     }
 }
